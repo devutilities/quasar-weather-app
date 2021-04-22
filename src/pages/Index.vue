bcdc20cef41bf3460c41d06bfcd8e555e9997794
Maitan <victormaitan01@gmail.com>
2021-04-22 02:31:35 -0400

    Removido códigos inuteis na index

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,11 +1,8 @@
 <template>
   <q-page class="flex flex-center">
     <img
-      v-for="item in contador"
-      :key="item"
       alt="Quasar logo"
       src="~assets/quasar-logo-full.svg"
-      @click="clique"
     />
   </q-page>
 </template>
@@ -15,14 +12,9 @@ export default {
   name: 'PageIndex',
   data () {
     return {
-      contador: 1,
-      i: 0
     }
   },
   methods: {
-    clique () {
-      alert('teste')
-    }
   }
 }
 </script>
