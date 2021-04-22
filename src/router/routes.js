c3eeca3219dcda51d6894d43469d35766187956e
Maitan <victormaitan01@gmail.com>
2021-04-22 01:47:39 -0400

    Registradas as rotas de login

--- src/router/routes.js
+++ src/router/routes.js
@@ -2,6 +2,13 @@
 const routes = [
   {
     path: '/',
+    component: () => import('layouts/LoginLayout.vue'),
+    children: [
+      { path: '', component: () => import('pages/Login.vue') }
+    ]
+  },
+  {
+    path: '/auth',
     component: () => import('layouts/MainLayout.vue'),
     children: [
       { path: '', component: () => import('pages/Index.vue') }
