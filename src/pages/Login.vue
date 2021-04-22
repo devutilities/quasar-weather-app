6ee57510a4ba845d0a46a745dc253247cfe24be6
Maitan <victormaitan01@gmail.com>
2021-04-22 01:47:18 -0400

    Criada tela de login

---
+++ src/pages/Login.vue
@@ -0,0 +1,22 @@
+<template>
+  <q-page class="flex flex-center">
+    <p class="text-h2">Login</p>
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'PageIndex',
+  data () {
+    return {
+      contador: 1,
+      i: 0
+    }
+  },
+  methods: {
+    clique () {
+      alert('teste')
+    }
+  }
+}
+</script>
