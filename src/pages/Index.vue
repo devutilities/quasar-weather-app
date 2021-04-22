dfe1c2dc6cb1a1d26c56d3caec00911b8bd3d303
Maitan <victormaitan01@gmail.com>
2021-04-22 01:13:25 -0400

    Initial Commit

---
+++ src/pages/Index.vue
@@ -0,0 +1,28 @@
+<template>
+  <q-page class="flex flex-center">
+    <img
+      v-for="item in contador"
+      :key="item"
+      alt="Quasar logo"
+      src="~assets/quasar-logo-full.svg"
+      @click="clique"
+    />
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'PageIndex',
+  data () {
+    return {
+      contador: 1,
+      i: 0
+    }
+  },
+  methods: {
+    clique () {
+      alert('teste')
+    }
+  }
+}
+</script>
